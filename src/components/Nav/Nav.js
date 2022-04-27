d8bba0ce4a070d4cd305fb4c651ae223665f1611
sseung-i <mschoise@gamil.com>
2022-04-27 16:16:52 +0900

    first

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Nav.scss";
 
 const Nav = () => {
-  return <div />;
+  return (
+    <nav className="nav">
+      <section className="navWrap">
+        <div className="logoWrap">
+          <img alt="logo" src="images/logo_color.png" />
+        </div>
+        <div className="rigintWrap">
+          <div className="menuWrap" />
+          <div className="btnWrap" />
+        </div>
+      </section>
+    </nav>
+  );
 };
 
 export default Nav;
