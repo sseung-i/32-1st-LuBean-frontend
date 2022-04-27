fe2be74ea2831952ab5c7cef20614e6878e018f8
jaewonyang <didwodnjs7268@gmail.com>
2022-04-27 11:30:31 +0900

    Modify: Main 페이지 기본 레이아웃 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -4,83 +4,109 @@ import "./Main.scss";
 const Main = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const container = useRef(null);
+  const button = useRef(null);
 
   function handleBtnClick(event) {
     setCurrentIndex(event.target.dataset.id);
   }
 
-  // setInterval(
-  //   useEffect(() => {
-  //     container.current.style.transform = `translate(-${currentIndex}00vw)`;
-  //     // container.current.classList.add("active");
-  //     // container.current.animation =
-  //   }, [currentIndex]),
-  //   3000
-  // );
-
-  if (currentIndex < 5) {
-    setCurrentIndex(currentIndex + 1);
-  }
-  function carousel() {
-    container.current.style.transform = `translate(-${currentIndex}00vw)`;
-  }
-
   useEffect(() => {
-    setInterval(carousel, 3000);
-    // container.current.classList.add("active");
-    // container.current.animation =
+    container.current.style.transform = `translate(-${currentIndex}00vw)`;
+    button.current.style.backgroundColor = "white";
   }, [currentIndex]);
+
+  // if (currentIndex < 5) {
+  //   setCurrentIndex(currentIndex + 1);
+  // }
+  // function carousel() {
+  //   container.current.style.transform = `translate(-${currentIndex}00vw)`;
+  // }
+
+  // useEffect(() => {
+  //   setInterval(carousel, 3000);
+  //   // container.current.classList.add("active");
+  //   // container.current.animation =
+  // }, [currentIndex]);
 
   return (
     <div className="main">
       {/* 위쪽 배너 슬라이드 */}
-      <div className="topWrapper">
-        <div ref={container} className="topContainer">
-          <div className="inner">
-            <img className="item" alt="item1" src="./images/slides/main1.jpg" />
-          </div>
-          <div className="inner">
-            <img className="item" alt="item2" src="./images/slides/main2.jpg" />
-          </div>
-          <div className="inner">
-            <img className="item" alt="item3" src="./images/slides/main3.jpg" />
-          </div>
-          <div className="inner">
-            <img className="item" alt="item4" src="./images/slides/main4.jpg" />
-          </div>
-          <div className="inner">
-            <img className="item" alt="item5" src="./images/slides/main5.jpg" />
+      <div className="WRAPPER">
+        <div className="topWrapper">
+          <div ref={container} className="topContainer">
+            <div className="inner">
+              <img
+                className="item"
+                alt="item1"
+                src="./images/slides/main1.jpg"
+              />
+            </div>
+            <div className="inner">
+              <img
+                className="item"
+                alt="item2"
+                src="./images/slides/main2.jpg"
+              />
+            </div>
+            <div className="inner">
+              <img
+                className="item"
+                alt="item3"
+                src="./images/slides/main3.jpg"
+              />
+            </div>
+            <div className="inner">
+              <img
+                className="item"
+                alt="item4"
+                src="./images/slides/main4.jpg"
+              />
+            </div>
+            <div className="inner">
+              <img
+                className="item"
+                alt="item5"
+                src="./images/slides/main5.jpg"
+              />
+            </div>
           </div>
         </div>
+        <div className="buttons">
+          <button
+            ref={button}
+            data-id="0"
+            onClick={handleBtnClick}
+            className={`btn${currentIndex}`}
+          />
+          <button
+            ref={button}
+            data-id="1"
+            onClick={handleBtnClick}
+            className={`btn${currentIndex}`}
+          />
+          <button
+            ref={button}
+            data-id="2"
+            onClick={handleBtnClick}
+            className={`btn${currentIndex}`}
+          />
+          <button
+            ref={button}
+            data-id="3"
+            onClick={handleBtnClick}
+            className={`btn${currentIndex}`}
+          />
+          <button
+            ref={button}
+            data-id="4"
+            onClick={handleBtnClick}
+            className={`btn${currentIndex}`}
+          />
+        </div>
       </div>
-      <button
-        data-id="0"
-        onClick={handleBtnClick}
-        className={`btn${currentIndex}`}
-      />
-      <button
-        data-id="1"
-        onClick={handleBtnClick}
-        className={`btn${currentIndex}`}
-      />
-      <button
-        data-id="2"
-        onClick={handleBtnClick}
-        className={`btn${currentIndex}`}
-      />
-      <button
-        data-id="3"
-        onClick={handleBtnClick}
-        className={`btn${currentIndex}`}
-      />
-      <button
-        data-id="4"
-        onClick={handleBtnClick}
-        className={`btn${currentIndex}`}
-      />
 
       {/* 가운데 상품 슬라이드 */}
-      <div className="bottomContainer">
+      <div className="productContainer">
         <h2 className="title">인기 원두 Top 12</h2>
         <div className="carousel">
           <div className="items">
@@ -105,9 +131,10 @@ const Main = () => {
         <div className="text">
           <h2 className="title">발리데이션 페이셜</h2>
           <p className="description">
-            발리데이션 페이셜 스파 트리트먼트'로 온몸의 감각을 깨워보세요.상의
-            보드라움이 피부 위에 내려앉은 듯한 느낌. 전문가의 능숙한 손길은
-            당신이 그리워하던 건강함을 되찾아줄 거예요.
+            발리데이션 페이셜 스파 트리트먼트'로 온몸의 감각을 <br />
+            깨워보세요. 상의 보드라움이 피부 위에 내려앉은 듯한 <br />
+            느낌. 전문가의 능숙한 손길은 당신이 그리워하던 건강함 <br />을
+            되찾아줄 거예요.
           </p>
         </div>
         <img
