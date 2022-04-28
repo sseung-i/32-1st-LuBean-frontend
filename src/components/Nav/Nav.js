70052a4cf8e78fb91b30d53ae15d71347183b106
sseung-i <mschoise@gamil.com>
2022-04-28 11:12:51 +0900

    add: 서브 카테고리 작업중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.scss";
 
+const CATEGORY = [
+  { id: 0, title: "사이트소개", name: "lubinSite" },
+  { id: 1, title: "원두", name: "products" },
+  { id: 2, title: "굿즈", name: "goods" },
+  { id: 3, title: "매장", name: "stores" },
+  { id: 4, title: "이벤트", name: "event" },
+];
+
+const SUBMENU = [
+  { id: 0, title: "싱글오리진" },
+  { id: 1, title: "블렌디드" },
+  { id: 2, title: "디카페인" },
+];
+
+const SINGLE_ORIGN = [""];
+
 const Nav = () => {
-  const category = [
-    { id: 0, title: "사이트소개", subMenu: [] },
-    {
-      id: 1,
-      title: "원두",
-      subMenu: [
-        { id: 0, title: "싱글오리진" },
-        { id: 0, title: "블렌디드" },
-        { id: 0, title: "디카페인" },
-      ],
-    },
-    { id: 2, title: "굿즈", subMenu: [] },
-    { id: 3, title: "매장", subMenu: [] },
-    { id: 4, title: "이벤트", subMenu: [] },
-  ];
+  const [nowClick, setNowClick] = useState("");
   return (
     <nav>
       <section className="navWrap">
@@ -25,13 +27,28 @@ const Nav = () => {
         </div>
         <div className="rigintWrap">
           <ul className="menuWrap">
-            {category.map(it => (
+            {CATEGORY.map(it => (
               <li key={it.id}>{it.title}</li>
             ))}
           </ul>
-          <div className="btnWrap" />
+          <div className="btnWrap">
+            <i className="fa-solid fa-xl fa-magnifying-glass" />
+            <div>
+              <span className="itemNum">0</span>
+              <i className="fa-solid fa-xl fa-basket-shopping" />
+            </div>
+            <i className="fa-solid fa-xl fa-circle-user" />
+          </div>
         </div>
       </section>
+      {/* <section key={it.id} className="dropMenu">
+        <ul>
+          <li>
+            <p className="">{it.title}</p>
+            <ul>{it.subMenu}</ul>
+          </li>
+        </ul>
+      </section> */}
     </nav>
   );
 };
